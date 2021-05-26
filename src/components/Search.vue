bab734e8c945c9b0e263831c098946396c450954
Dane Pedersen <dane@Danes-MBP.attlocal.net>
2021-05-26 11:37:35 -0500

    First Commit

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,37 +1,80 @@
 <template>
   <div class="search">
-    <h2>Type In Your Search Term</h2>
+    <!-- <h2>Type In Your Search Term</h2>
+    <form v-on:submit.prevent="getResult(query)"> -->
+      <h2>Browse a subreddit</h2>
     <form v-on:submit.prevent="getResult(query)">
       <input type="text" placeholder="Type in your search" v-model="query" />
     </form>
+    <h2 class="errorMessage">{{ errormessage }}</h2>
     <div class="results" v-if="results">
-
+      <br>
+      <i>Browsing: {{ query }}</i>
       <div v-for="result in results">
-          <img v-bind:src="result.links[0].href" />
+          <h2 class="title"><a v-bind:href="result.data.url" />{{ result.data.title }}</h2>
+          <p class="score">Score: {{result.data.score}}</p>
+          <p class="updown">Upvotes: {{result.data.ups}} Downvotes: {{result.data.downs}}</p> 
+            <div v-if="result.data.url_overridden_by_dest">
+          <p class="subredditType">Subreddit Type: {{result.data.subreddit_type}}</p> 
+              <img v-bind:src="result.data.url_overridden_by_dest"/> 
+            </div>
+            <div v-else>
+              
+            </div>
+          <p class="selftext">{{result.data.selftext}}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+
+          
+//  <img v-bind:src="result.links[0].href" />
+
+// axios.get('https://images-api.nasa.gov/search?q=' + query + '&media_type=image').then( response => {
+//             console.log(response.data.collection.items);
+//             this.results = response.data.collection.items;
+//         });
 import axios from 'axios';
 export default {
   name: 'search',
   data () {
     return {
-      msg: 'Search',
+      msg: 'Enter a subreddit that you would like to browse',
       query: '',
-      results: ''
+      results: '',
+      errormessage: ''
     }
   },
   methods: {
       getResult(query) {
-        axios.get('https://images-api.nasa.gov/search?q=' + query + '&media_type=image').then( response => {
-            console.log(response.data.collection.items);
-            this.results = response.data.collection.items;
-        });
+        axios.get("https://www.reddit.com/r/" + query + ".json")
+        .then( response => {
+            console.log(response.data.data.children);
+            // console.log(response.data.data.children.preview.images);
+            this.results = response.data.data.children;
+            this.errormessage = '';
+            }).catch((error) => { 
+              console.warn('Not good man :(');
+              this.results = '';
+              this.errormessage = "Sorry, I couldn't find that subreddit. Please try again"
+            });
+            /*
+            Need to loop through all of the returns and display the info 
+            this.results = response.data.data.children[0].data; ++
+            */
+        
       }
   }
+  // methods: {
+  //     getResult(query) {
+  //       axios.get('https://images-api.nasa.gov/search?q=' + query + '&media_type=image').then( response => {
+  //           console.log(response.data.collection.items);
+  //           this.results = response.data.collection.items;
+  //       });
+  //     }
+  // }
 }
 </script>
 
